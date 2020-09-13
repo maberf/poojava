ba8dc8f4c6c77c47a74450cb104535e1b6b2c04e
Maber Fernandes <71026050+maberf@users.noreply.github.com>
2020-09-13 11:56:58 -0300

    Aula 06

---
+++ aula06/src/main/java/aula06/Aula06.java
@@ -0,0 +1,20 @@
+package aula06;
+public class Aula06 {
+public static void main(String[] args) {
+// CONTROLE REMOTO
+// Uso através da interface (ENCAPSULAMENT0)
+// Getters e Setters são privados, mas o Construtor é público (ainda...)
+    
+ControleRemoto c = new ControleRemoto();
+   // Agora pode-se "brincar" com os controles da interface Controlador. Atenção aos valores iniciais no Construtor
+   // Perceber a simplicidade e intuição dos comandos
+    c.ligar();
+    // c.desligar(); // desligo o aparelho para simular uma mensagem de erro no volume
+    c.maisVolume(); 
+    c.play();
+    c.abrirMenu();
+    c.fecharMenu();
+    
+    }
+    
+}
